ab0de5770df52d37caccd343081735da4226e761
ThanVanDanhh <23130043@st.hcmuaf.edu.vn>
2026-05-06 18:24:43 +0700

    first commit

---
+++ src/main/java/vn/edu/nlu/fit/minesweeper/Launcher.java
@@ -0,0 +1,9 @@
+package vn.edu.nlu.fit.minesweeper;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
