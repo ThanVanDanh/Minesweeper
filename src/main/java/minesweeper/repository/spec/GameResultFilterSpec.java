35567651dd353741752d5e11557d625c2695a93e
Nhu1Duy <23130078@st.hcmuaf.edu.vn>
2026-05-28 22:41:07 +0700

    cập nhập filter class cho User và Result

---
+++ src/main/java/minesweeper/repository/spec/GameResultFilterSpec.java
@@ -0,0 +1,9 @@
+package minesweeper.repository.spec;
+
+import minesweeper.model.enums.Difficulty;
+
+public class GameResultFilterSpec {
+    public String     username;
+    public Difficulty difficulty;
+    public Boolean    win;
+}
