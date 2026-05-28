b3e2d625302a82b9e7765f5e8f51e246d0840265
Nhu1Duy <23130078@st.hcmuaf.edu.vn>
2026-05-28 22:41:07 +0700

    cập nhập filter class cho User và Result

---
+++ src/main/java/minesweeper/repository/spec/UserFilterSpec.java
@@ -0,0 +1,9 @@
+package minesweeper.repository.spec;
+
+import minesweeper.model.enums.Role;
+
+public class UserFilterSpec {
+    public String  keyword;
+    public Role role;
+    public Boolean active;
+}
