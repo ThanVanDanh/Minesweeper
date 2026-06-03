da97751691dc86e24b7227541f4b2525f0848c9b
ThanVanDanhh <23130043@st.hcmuaf.edu.vn>
2026-06-03 22:26:03 +0700

    tim kiem nang cao user + test case

--- src/main/java/minesweeper/repository/spec/UserFilterSpec.java
+++ src/main/java/minesweeper/repository/spec/UserFilterSpec.java
@@ -2,6 +2,8 @@ package minesweeper.repository.spec;
 
 import minesweeper.model.enums.Role;
 
+import java.time.LocalDate;
+
 /**
  * Điều kiện lọc user.
  * Trường nào null → không áp dụng điều kiện đó.
@@ -9,8 +11,16 @@ import minesweeper.model.enums.Role;
 public class UserFilterSpec {
 
     public String  keyword;
+    public String  email;
     public Role    role;
     public Boolean active;
+    public LocalDate createdFrom;
+    public LocalDate createdTo;
+    public LocalDate lastLoginFrom;
+    public LocalDate lastLoginTo;
+    public Integer minGames;
+    public Integer maxGames;
+    public Boolean hasGameResults;
 
     public UserFilterSpec() {}
 
